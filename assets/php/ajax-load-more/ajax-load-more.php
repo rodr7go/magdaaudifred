8b4df94a6cc24c6c6ecc25aaa8f68f38aa49d038
Rodrigo Lopez <rodr7go.lg@gmail.com>
2018-01-23 15:14:41 -0600

    first commit

---
+++ assets/php/ajax-load-more/ajax-load-more.php
@@ -0,0 +1,147 @@
+
+<div id="works-grid" class="works-grid works-hover-w">
+
+	<!-- DO NOT DELETE THIS DIV -->
+	<div class="grid-sizer"></div>
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item tall branding">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-1.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">The European languages</h3>
+				<div class="work-descr">
+					Branding
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item design">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-2.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">The languages only</h3>
+				<div class="work-descr">
+					Design
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item design">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-3.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">Everyone realizes</h3>
+				<div class="work-descr">
+					Design
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item design">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-4.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">Corporate Identity</h3>
+				<div class="work-descr">
+					Design
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item design">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-5.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">Cambridge friend</h3>
+				<div class="work-descr">
+					Design
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item wide-tall branding photo web">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-6.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">The grammar of the resulting language</h3>
+				<div class="work-descr">
+					Branding / Photo / Web
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item wide design photo web">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-7.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">A collection of textile samples</h3>
+				<div class="work-descr">
+					Design / Photo / Web
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item branding photo">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-8.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">Gregor then turned</h3>
+				<div class="work-descr">
+					Branding / Photo
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item branding photo">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-9.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">He must have tried it</h3>
+				<div class="work-descr">
+					Branding / Photo
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+	<!-- PORTFOLIO ITEM -->
+	<div class="work-item wide design web">
+		<a href="portfolio-single-1.html">
+			<img src="assets/images/portfolio/img-10.jpg" alt="">
+			<div class="work-caption font-alt">
+				<h3 class="work-title">However hard he threw himself</h3>
+				<div class="work-descr">
+					Design / Web
+				</div>
+			</div>
+		</a>
+	</div>
+	<!-- /PORTFOLIO ITEM -->
+
+</div>
