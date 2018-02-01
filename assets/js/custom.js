c7f0f1651e3c5420432b71d8a009284a9889e729
Rodrigo Lopez <rodr7go.lg@gmail.com>
2018-02-01 15:40:46 -0600

    galeria integrada

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -545,19 +545,11 @@
 			return false;
 		});
 
+		//------- light gallery -------//
 
-        // anime({
-        //     targets: '.main-logo',
-        //     translateX: [
-        //         { value: 100, duration: 1200 },
-        //         { value: 0, duration: 800 }
-        //     ],
-        //     rotate: '1turn',
-        //     // backgroundColor: '#FFF',
-        //     duration: 2000,
-        //     loop: true
-        // });
-
+		$("#aniimated-thumbnials").lightGallery({
+			thumbnail:true
+		});
 
 		// END
     });
